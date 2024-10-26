581a6f96b619d5321aa29ec7928d57ee992b26f7
Le Thanh Tung <tunglt042@gmail.com>
2024-10-26 11:30:54 +0700

    chore: add midleware for token expried

--- src/utils/ApiUtils.ts
+++ src/utils/ApiUtils.ts
@@ -30,3 +30,16 @@ axiosInstance.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("accessToken");
+      window.location.href = "/auth/login";
+    }
+    return Promise.reject(error);
+  }
+);
